5faa7c5c470e54320e7f96155c9dd59f485f323e
rcalazansn <rcalazansn@gmail.com>
2019-09-28 14:32:10 -0300

    adicionado modulo I (basico)

---
+++ modulo I/exercicios/exercicios/Exemplos.cs
@@ -0,0 +1,108 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicios
+{
+    public class Exemplos
+    {
+        public void FatorialNumeroQuatro()
+        {
+            int fatorial = 4 * 3 * 2 * 1;
+            Console.WriteLine($"Fatorial quatro: 4 X 3 X 2 X 1 = {fatorial}");
+        }
+
+        public int FatorialNumeroCinco()
+        {
+            int fatorial = 5 * 4 * 3 * 2 * 1;
+            return fatorial;
+        }
+
+        public int FatorialQualquerNumero(int numero)
+        {
+            int resultado = 1;
+
+            for (int i = 1; i <= numero; i++)
+            {
+                resultado = resultado * i;
+            }
+
+            return resultado;
+        }
+
+        public void MaiorNumero(int numero1, int numero2)
+        {
+            if (numero1 > numero2)
+            {
+                Console.WriteLine($"{numero1} e maior que {numero2}");
+            }
+            else if (numero2 > numero1)
+            {
+                Console.WriteLine($"{numero2} e maior que {numero1}");
+            }
+            else
+            {
+                Console.WriteLine("sao igual os valores");
+            }
+        }
+
+        public void NumeroEhImpar(int numero)
+        {
+            if (numero % 2 != 0)
+                Console.WriteLine("O numero e IMPAR");
+            else
+                Console.WriteLine("O numero e PAR");
+        }
+
+        public void Calculadora(int valor1, int valor2, string operacao)
+        {
+            switch (operacao)
+            {
+                case "/":
+                    Console.WriteLine($"{valor1} / {valor2} = {(valor1 / valor2)}");
+                    break;
+                case "*":
+                    Console.WriteLine($"{valor1} * {valor2} = {(valor1 * valor2)}");
+                    break;
+                case "-":
+                    Console.WriteLine($"{valor1} - {valor2} = {(valor1 - valor2)}");
+                    break;
+                case "+":
+                    Console.WriteLine($"{valor1} + {valor2} = {(valor1 + valor2)}");
+                    break;
+                default:
+                    Console.WriteLine("Nao foi informada uma operação valida!");
+                    break;
+            }
+        }
+
+        public void Tabuada(int tabuada)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} * {tabuada} = {i * tabuada}");
+            }
+        }
+
+        public void Temperatura(int temperatura, string operacao)
+        {
+            decimal t = 0;
+            switch (operacao.ToLower())
+            {
+                case "f":
+                    t = (((temperatura * 9) / 5) + 32);
+                    Console.WriteLine($"{temperatura}");
+                    break;
+                case "c":
+                    t = (((temperatura - 32) * 5) / 9);
+                    break;
+                default:
+                    Console.WriteLine("Nao foi informada uma oepracao valida");
+                    break;
+            }
+
+            Console.WriteLine($"{temperatura}");
+        }
+    }
+}
---
+++ modulo I/exercicios/exercicios/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Hello World 1");
+             Console.WriteLine("Hello World 2");
+
+             string nome;
+             bool inativo;
+             int numero;
+             long numero_1;
+             decimal valor = 10_000_00;
+             decimal valor_1 = 20.00M;
+             DateTime hoje = DateTime.Now; //Data Atual
+             DateTime data_manual = new DateTime(2019, 09, 28); //Data Manual
+             string hoje_formadata = hoje.ToString("dd/MM/yyyy HH:mm:ss"); //Formatação
+             string hoje_formadata_2 = hoje.ToString("T"); //Time
+             char tipo;
+
+             int[] array_int = new int[5];
+
+
+             string[] array_string = new string[5];
+             array_string[0] = "A";
+             array_string[1] = "B";
+             array_string[2] = "C";
+             array_string[3] = "D";
+             array_string[4] = "E";
+
+             Console.WriteLine(">>" + hoje_formadata);
+             Console.WriteLine($">> {hoje_formadata_2} <<");
+             Console.WriteLine($">> {valor.ToString()} <<");
+
+             string variavel_usuario = Console.ReadLine();
+
+             // Console.WriteLine($"Variavel:{Console.ReadLine()}");
+             // Console.WriteLine($"Variavel:{variavel_usuario}");
+
+             if (variavel_usuario.ToLower() == "calazans")
+             {
+                 Console.WriteLine("nome igual calazans");
+             }
+             else
+             {
+                 Console.Write("nome diferente de calazans,");
+                 Console.Write($"o nome informado foi:{variavel_usuario}");
+             }
+
+             Console.WriteLine($"Tamanho: {variavel_usuario.Length}");
+             Console.WriteLine($"Tamanho: {variavel_usuario.PadLeft(10, '-')}");
+             Console.WriteLine($"Tamanho: {variavel_usuario.PadRight(10, '*')}");*/
+
+            Exemplos exemplo = new Exemplos();
+            /*
+            exemplo.FatorialNumeroQuatro();
+
+            Console.WriteLine();
+            int retorno = exemplo.FatorialNumeroCinco();
+            Console.WriteLine($"Fatorial cinco={retorno}");
+
+            Console.WriteLine();
+            Console.Write("Informe o fatorial que deseja:");
+            string numeroUsuario = Console.ReadLine();
+            int numeroFatorial = Convert.ToInt32(numeroUsuario);
+            Console.WriteLine($"FatorialQualquerNumero:{exemplo.FatorialQualquerNumero(numeroFatorial)}");
+
+            Console.WriteLine();
+            Console.Write("Informa o primeiro valor:");
+            int valor1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informa o segundo valor:");
+            int valor2 = Convert.ToInt32(Console.ReadLine());
+            exemplo.MaiorNumero(valor1, valor2);
+
+            Console.WriteLine();
+            Console.Write("Qual numero deseja saber se e impar ou par:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            exemplo.NumeroEhImpar(numero);
+            */
+
+            /*
+            Console.WriteLine();
+            Console.Write("primeiro valor:");
+            int valorCalculadora1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("segundo valor:");
+            int valorCalculadora2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Qual operacao [/ * - +]:");
+            string operacao = Console.ReadLine();
+            exemplo.Calculadora(valorCalculadora1, valorCalculadora2, operacao);
+            */
+
+            /*Console.WriteLine();
+            Console.Write("Tabuada:");
+            int tabuada = Convert.ToInt32(Console.ReadLine());
+            exemplo.Tabuada(tabuada);*/
+
+            Console.WriteLine();
+            Console.Write("Temperatura:");
+            int temperatura = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Celsius => Fahrenheit = [C]");
+            Console.Write("Fahrenheit => Celsius= [F]");
+            string operacao = Console.ReadLine();
+            exemplo.Temperatura(temperatura, operacao);
+        }
+    }
+}
