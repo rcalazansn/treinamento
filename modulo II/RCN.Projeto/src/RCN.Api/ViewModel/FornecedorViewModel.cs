2ecc64764dec0003a275d383aba0888afd9c5030
rcalazansn <rcalazansn@gmail.com>
2019-12-16 21:39:55 -0300

    RCN.API atualizacao

--- modulo II/RCN.Projeto/src/RCN.Api/ViewModel/FornecedorViewModel.cs
+++ modulo II/RCN.Projeto/src/RCN.Api/ViewModel/FornecedorViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using RCN.Business.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,10 +10,53 @@ namespace RCN.Api.ViewModel
     public class FornecedorViewModel
     {
         public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
         public string Documento { get; set; }
+
         public int TipoFornecedor { get; set; }
+
         public bool Ativo { get; set; }
-        public IEnumerable<Produto> Produtos { get; set; }
+
+        public IEnumerable<ProdutoViewModel> Produtos { get; set; }
+    }
+
+    public class FornecedorAdicionarViewModel
+    {
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
+        public string Documento { get; set; }
+
+        public int TipoFornecedor { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+
+    public class FornecedorEditarViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
+        public string Documento { get; set; }
+
+        public int TipoFornecedor { get; set; }
+
+        public bool Ativo { get; set; }
     }
 }
--- modulo II/RCN.Projeto/src/RCN.Api/ViewModel/ProdutoViewModel.cs
+++ modulo II/RCN.Projeto/src/RCN.Api/ViewModel/ProdutoViewModel.cs
@@ -5,9 +5,35 @@ namespace RCN.Api.ViewModel
 {
     public class ProdutoViewModel
     {
-        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public Guid Id { get; set; }
 
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+
+        public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Descricao { get; set; }
+
+        public string ImagemUpload { get; set; }
+
+        public string Imagem { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public decimal Valor { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+    }
+
+    public class ProdutoAdicionarViewModel
+    {
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public Guid FornecedorId { get; set; }
 
@@ -25,6 +51,31 @@ namespace RCN.Api.ViewModel
         public decimal Valor { get; set; }
 
         public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+    }
+
+    public class ProdutoEditarViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Descricao { get; set; }
+
+        public string Imagem { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public decimal Valor { get; set; }
 
         public bool Ativo { get; set; }
     }
